38200b6558ca7ab2570d9947a7644eb4148383bf
MYAknight <75816688+MYAknight@users.noreply.github.com>
2022-10-22 14:00:48 +0800

    still a committttttttt

--- EE308_LAB1/src/main/java/TASK2.java
+++ EE308_LAB1/src/main/java/TASK2.java
@@ -1,4 +1,11 @@
+import org.junit.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+
 import java.io.*;
+import java.util.Stack;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -6,13 +13,12 @@ public class TASK2 {
 
 
     public static void main(String[] args) throws Exception {
-        sol("C:\\Users\\huaqi\\Desktop\\LAB.c", 2);
-
-
+        TASK2 task2 = new TASK2();
+        task2.sol("C:\\Users\\huaqi\\Desktop\\LAB.c", 4);//this is your url
     }
 
-
-    public static void sol(String url, int level) throws IOException {
+    @Test
+    public void sol(String url, int level) throws IOException {
         File file = new File(url);
         BufferedReader br
                 = new BufferedReader(new FileReader(file));
@@ -26,38 +32,83 @@ public class TASK2 {
         if (level == 1) {
             sol1(s2);
         } else if (level == 2) {
-            sol1(s2);
             sol2(s2);
-        }else if (level==3){
-
-        }else if (level==4){
-
+        } else if (level == 3) {
+            sol3(s2);
+        } else if (level == 4) {
+            sol4(s2);
         }
     }
 
-    /*  public static int sol1(int a, String base, String target) {
-          int count = 0;
-          int p = base.indexOf(target, a);
-          if (p == -1) {
-              return count;
-          } else {
-              return 1 + sol1(p + target.length(), base, target);
-          }
-      }*/
-    public static void sol1(String base) {
+    @Test
+    public void sol3(String base) {
+        sol2(base);
+        int count = match2(base, 3);
+        System.out.println("if-else num: " + count);
+    }
+
+    @Test
+    @ParameterizedTest
+    @ValueSource(strings = {"if(){}else{}if{}else if{}else{}if{}if{}"})
+    public void sol4(String base) {
+        sol3(base);
+        int count = match2(base, 4);
+        System.out.println("if-elseif-else num: " + count);
+    }
+
+    @Test
+    public int match2(String str, int level) {
+        Stack<String> s1 = new Stack();
+        Pattern pattern = Pattern.compile("else *if|else|if");
+        Matcher matcher = pattern.matcher(str);
+        int esif = 0;
+        int ifes = 0;
+        boolean temp = true;
+        while (matcher.find()) {
+            String substring = str.substring(matcher.start(), matcher.end());
+            if ("if".equals(substring)) {
+                s1.push(substring);
+            } else if ("else".equals(substring)) {
+                while (!"if".equals(s1.peek())) {
+                    String pop = s1.pop();
+                    if (temp) {
+                        esif++;
+                        temp = false;
+                    }
+                }
+                if (temp == true) {
+                    ifes++;
+                }
+                s1.pop();
+                temp = true;
+            } else {
+                s1.push(substring);
+            }
+        }
+        if (level == 3) {
+            return ifes;
+        } else {
+            return esif;
+        }
+    }
+
+    @Test
+    public void sol1(String base) {
         int count = 0;
         String keyword[] = {"auto", "break", "case", "char", "const", "continue", "default", "do", "double", "else", "enum", "extern",
                 "float", "for", "goto", "if", "int", "long", "register", "return", "short", "signed", "sizeof", "static",
                 "struct", "switch", "typedef", "unsigned", "union", "void", "volatile", "while"};
         for (int i = 0; i < keyword.length; i++) {
-            String temp = "[^a-zA-Z_]"+keyword[i]+"[^a-zA-Z_]";
+            String temp = "[^a-zA-Z_]" + keyword[i] + "[^a-zA-Z_]";
             int match = match(base, temp);
             count += match;
         }
         System.out.println("total num: " + count);
     }
 
-    public static void sol2(String base) {
+    @Test
+    public void sol2(String base) {
+        sol1(base);
         System.out.println("switch num: " + match(base, "switch"));
         String[] temp = base.split("switch");
         System.out.print("case num: ");
@@ -67,7 +118,10 @@ public class TASK2 {
         System.out.println();
     }
 
-    public static int match(String str, String target) {
+
+    @Test
+    public int match(String str, String target) {
+
         Pattern pattern = Pattern.compile(target);//匹配一个或多个数字字符
         Matcher matcher = pattern.matcher(str);
         int count = 0;
